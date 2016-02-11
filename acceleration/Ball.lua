be868e29b089c6776ac370a1606d49a8ede402c6
Erick <erickzanardoo@gmail.com>
2016-02-11 13:23:16 -0200

    Simplifying acceleration :D

--- acceleration/Ball.lua
+++ acceleration/Ball.lua
@@ -7,27 +7,18 @@ function Ball(pos)
   }
 
   function ball:update(dt)
-    local xDirection
-    local yDirection
-
-    if self.speed.x < 0 then xDirection = -1 else xDirection = 1 end
-    if self.speed.y < 0 then yDirection = -1 else yDirection = 1 end
-
-    self.speed = self.speed:abs()
     self.speed = self.speed:add(self.acceleration:multiply(dt))
-
-    self.speed.x = self.speed.x * xDirection
-    self.speed.y = self.speed.y * yDirection
-
     self.pos = self.pos:add(self.speed:multiply(dt))
   end
 
   function ball:checkBounds(area)
     if self.pos.x + self.radius > area.width or self.pos.x - self.radius < 0 then
       self.speed.x = self.speed.x * -1
+      self.acceleration.x = self.acceleration.x  * -1
     end
     if self.pos.y + self.radius > area.height or self.pos.y - self.radius < 0 then
       self.speed.y = self.speed.y * -1
+      self.acceleration.y = self.acceleration.y  * -1
     end
   end
 
