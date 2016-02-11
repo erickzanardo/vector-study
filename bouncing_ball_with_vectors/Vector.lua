85d450390af4473eb6addcc4cb4c1c29ffaf6a14
Erick <erickzanardoo@gmail.com>
2016-02-10 22:44:03 -0200

    asda

--- bouncing_ball_with_vectors/Vector.lua
+++ bouncing_ball_with_vectors/Vector.lua
@@ -1,16 +1,1 @@
-function Vector(x, y)
-  local vector = {
-    x = x;
-    y = y
-  }
-
-  function vector:add(vector)
-    return Vector(self.x + vector.x, self.y + vector.y)
-  end
-
-  function vector:scalarMultiply(value)
-    return Vector(self.x * value, self.y * value)
-  end
-
-  return vector
-end
+../Vector.lua
